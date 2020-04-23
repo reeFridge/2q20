e08e209d71784adafdc3368baa716150749bbfa9
fridge <reefridgerator@gmail.com>
2020-04-24 02:46:18 +0700

    refactoring fade transition (sfm-like)

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,17 @@ extends Node
 
 var current_scene: Node2D = null
 var next_scene: Node2D = null
-var scene_transition = null
+
+enum TransitionPhase {
+	NONE,
+	FADE_IN,
+	FADE_OUT
+}
+
+var scene_transition_phase = TransitionPhase.NONE
+
+func _ready():
+	$ui.show_fading_rect()
 
 func get_player():
 	if current_scene != null:
@@ -11,35 +21,43 @@ func get_player():
 	return null
 
 func set_scene(scene):
-	$CanvasLayer.get_node("bg").visible = true
 	if current_scene == null:
-		current_scene = scene
-		add_child(current_scene)
-		$AnimationPlayer.play_backwards("bg_fade")
-	elif scene.fade == false:
-		$CanvasLayer.get_node("bg").visible = false
-		if current_scene != null:
-			remove_child(current_scene)
-		current_scene = scene
-		add_child(current_scene)
-	else:
 		next_scene = scene
-		$AnimationPlayer.play("bg_fade")
-		scene_transition = true
+		next_transition_phase(TransitionPhase.FADE_IN)
+		pass
+
+	if scene.fade:
+		next_scene = scene
+		next_transition_phase()
+	else:
+		$ui.hide_fading_rect()
+		next_scene = scene
+		perform_transition()
+
+func perform_transition():
+	if current_scene != null:
+		remove_child(current_scene)
+	current_scene = next_scene
+	next_scene = null
+	add_child(current_scene)
+	
+func next_transition_phase(current_phase = scene_transition_phase):
+	match current_phase:
+		TransitionPhase.NONE:
+			$ui.show_fading_rect()
+			$AnimationPlayer.play("fg_fade")
+			scene_transition_phase = TransitionPhase.FADE_IN
+		TransitionPhase.FADE_IN:
+			perform_transition()
+			$AnimationPlayer.play_backwards("fg_fade")
+			scene_transition_phase = TransitionPhase.FADE_OUT
+		TransitionPhase.FADE_OUT:
+			$ui.hide_fading_rect()
+			scene_transition_phase = TransitionPhase.NONE
 
 func _on_animation_finished(anim_name):
-	if anim_name == "bg_fade":
-		if scene_transition != null:
-			if scene_transition:
-				if current_scene != null:
-					remove_child(current_scene)
-				current_scene = next_scene
-				next_scene = null
-				add_child(current_scene)
-				$AnimationPlayer.play_backwards("bg_fade")
-				scene_transition = false
-			else:
-				$CanvasLayer.get_node("bg").visible = false
-				scene_transition = null
+	if anim_name == "fg_fade":
+		if scene_transition_phase == TransitionPhase.NONE:
+			$ui.hide_fading_rect()
 		else:
-			$CanvasLayer.get_node("bg").visible = false
+			next_transition_phase()
---
+++ scripts/ui.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+func hide_fading_rect():
+	$fg.visible = false
+	
+func show_fading_rect():
+	$fg.visible = true
